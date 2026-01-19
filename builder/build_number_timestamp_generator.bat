9487a51e75bfc3c373c8919c48fb98f8a82230b7
Stefan Schindler <schindler.s@mail.de>
2026-01-19 18:09:21 +0100

    improve build generation + add config

--- builder/build_number_timestamp_generator.bat
+++ builder/build_number_timestamp_generator.bat
@@ -2,22 +2,17 @@
 setlocal ENABLEDELAYEDEXPANSION
 
 rem === CONFIGURATION ===
-set "TEMPLATE_FILE=01-version.source.filter"
-set "OUTPUT_FILE=01-version.filter"
-set "BUILDNUM_FILE=buildnum.txt"
+set "OUTPUT_FILE=01-version-aliases.filter"
 
-rem === GET CURRENT BUILD NUMBER (PERSISTENT) ===
-if not exist "%BUILDNUM_FILE%" (
-    echo 0>"%BUILDNUM_FILE%"
+rem === GET CURRENT BUILD NUMBER FROM EXISTING FILE OR START AT 1 ===
+if exist "%OUTPUT_FILE%" (
+    for /f "tokens=3" %%i in ('findstr "FILTER_BUILDNUM" "%OUTPUT_FILE%"') do set CURRENT_BUILDNUM=%%i
+    if "%CURRENT_BUILDNUM%"=="" set CURRENT_BUILDNUM=0
+) else (
+    set CURRENT_BUILDNUM=0
 )
 
-set /p CURRENT_BUILDNUM=<"%BUILDNUM_FILE%"
-if "%CURRENT_BUILDNUM%"=="" set CURRENT_BUILDNUM=0
-
 set /a NEW_BUILDNUM=CURRENT_BUILDNUM+1
-
-rem Save the new build number for next run
-> "%BUILDNUM_FILE%" echo %NEW_BUILDNUM%
 
 rem === GET FORMATTED TIMESTAMP LIKE Nov/25th/25 ===
 for /f "usebackq delims=" %%T in (`
@@ -41,18 +36,9 @@ for /f "usebackq delims=" %%T in (`
 )
 
 rem === DO REPLACEMENT AND WRITE OUTPUT ===
-if not exist "%TEMPLATE_FILE%" (
-    echo Template file "%TEMPLATE_FILE%" not found.
-    exit /b 1
-)
-
 (
-    for /f "usebackq delims=" %%L in ("%TEMPLATE_FILE%") do (
-        set "LINE=%%L"
-        set "LINE=!LINE:--timestamp--=%TIMESTAMP%!"
-        set "LINE=!LINE:--buildnum--=%NEW_BUILDNUM%!"
-        echo(!LINE!
-    )
+    echo #define FILTER_TIMESTAMP %TIMESTAMP%
+    echo #define FILTER_BUILDNUM %NEW_BUILDNUM%
 ) > "%OUTPUT_FILE%"
 
 echo Done.
